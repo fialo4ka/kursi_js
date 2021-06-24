9d2ebcb83fbc0317a7915b0209f2497da5675163
fialo4ka <tona16@inbox.lv>
2021-06-24 21:40:15 +0200

    read me

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Womens kurss JavScript
+Initial project to start working with JavScript
+
+Structure:
+    |_ browser_js  => try to use js in html page
+    |_ node_js => try to use js in Node.js
+    |_ test => try to simple web page QA testing with Node.js, Mocha, Chai
+
+
+### How to run simple js example in html page:
+
+open simple.html in any browser
+
+### How to run simple js code in node.js:
+
+- open Node.js console from start menu 
+- for Windows open cmd
+- for Mac open terminal
+- in VScode you can use build in terminal
+
+1. Check if your pc has Node.js with
+```node -v```
+2. run command
+```node .<path to js file>/<file name>.js```
+```node ./node_js/first.js```
+
+### How to run simple js code in node.js:
+
+using any terninal (as in previos)
+
+##### First running
+1. check if you have npm instaled, if it isn't show you a version install npm 
+```npm -v```
+2. in project folder 
+``` npm install```
+it will create node_modules folder
+3. Run test 
+```npm test```
--- test/server.js
+++ test/server.js
@@ -5,27 +5,25 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("Get fialo.info data. if it works?", function() {  
-    let url = "https://fialo.info/"
+    let url = "https://fialo.info"
     it("returns status 200", (done) => {
         chai.request(url)
             .get('/')
             .end((err, res) => {
-                    res.should.have.status(200);
+                res.should.have.status(200);
                 done();
             });
     });
   });
 
-describe("Get inara.cc data. if it works?", function() {
-    let url = "https://inara.cc/"
-    it("returns status 200", function() {
-        request(url, (done) => {
-            chai.request(url)
-                .get('/')
-                .end((err, res) => {
-                        res.should.have.status(200);
-                    done();
-                });
-      });
-    });  
+  describe("Get inara.cc data. if it works?", function() {  
+    let url = "https://inara.cc"
+    it("returns status 200", (done) => {
+        chai.request(url)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
   });
